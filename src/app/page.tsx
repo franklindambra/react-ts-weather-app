056dc1d86a424455e62e6ba11739108c4db84acc
Franklin Dambra <franklindambra@Franklins-MacBook-Pro.local>
2024-11-07 15:20:00 -0600

    new push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import React, {Suspense} from "react";
 import Weather from "@/components/Weather";
-
-// Spinner component
-const LoadingSpinner = () => (
-  <div className="flex justify-center items-center h-full">
-    <div className="animate-spin border-t-4 border-blue-500 border-solid w-16 h-16 rounded-full"></div>
-  </div>
-);
 
 export default function Home() {
   return (
